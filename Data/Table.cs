65edbb6076c7e2b7d60821d7115b19cb58cd66da
247813 Tomalak Piotr <247813@edu.p.lodz.pl>
2024-04-03 19:23:46 +0200

    Basic implementaion of LogicAPI + small changes in Data Layer.

--- Data/Table.cs
+++ Data/Table.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.ObjectModel;
-
-namespace Data
+﻿namespace Data
 {
     public class Table
     {
         public int Width { get; set; }
         public int Height { get; set; }
 
-        private ObservableCollection<Ball> _Balls;
-
         public Table(int width, int height)
         {
-            _Balls = new ObservableCollection<Ball>();
             Width = width;
             Height = height;
         }
---
+++ Logic/LogicAPI.cs
@@ -0,0 +1,13 @@
+﻿using Data;
+using System;
+using System.Collections.ObjectModel;
+
+namespace Logic
+{
+    public class LogicAPI
+    {
+        private readonly DataAPI _dataAPI;
+        public Table table { get }
+        public ObservableCollection<Ball> balls { get; } = new ObservableCollection<Ball>();
+    }
+}
