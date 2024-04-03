f358b5523e53919715db02470e1c1f059a289ac1
hjatczak <hjatczak@gmail.com>
2024-04-03 18:58:41 +0200

    Dodana clasa BallLogic.cs

---
+++ Logic/BallLogic.cs
@@ -0,0 +1,48 @@
+﻿using Data;
+using System.Numerics;
+
+namespace Logic
+{
+    public class BallLogic
+    {
+        private Ball _ball;
+        private static Table _table;
+
+        public float X
+        {
+            get { return _ball.Position.X; }
+        }
+
+        public float Y
+        {
+            get { return _ball.Position.Y; }
+        }
+
+        public int Radius
+        {
+            get { return _ball.Radius; }
+        }
+
+        public BallLogic(Ball ball)
+        {
+            _ball = ball;
+        }
+
+        public static void SetTable(Table table)
+        {
+            _table = table;
+        }
+
+        public Vector2 Velocity
+        {
+            get 
+            { 
+                return _ball.Velocity; 
+            }
+            set 
+            { 
+                _ball.Velocity = value;
+            }
+        }
+    }
+}
