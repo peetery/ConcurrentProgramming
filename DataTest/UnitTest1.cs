8d9c0677663bcfcdedb2fba3434a245239c38c4e
247813 Tomalak Piotr <247813@edu.p.lodz.pl>
2024-04-03 14:01:07 +0200

    Delete ConcurrentProgramming Directory and base application. Prepare new files structure.

---
+++ DataTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Datatest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
