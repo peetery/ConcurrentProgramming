aa71ffb469af85e384994eca65e6ae09b0df07a9
247813 Tomalak Piotr <247813@edu.p.lodz.pl>
2024-04-05 17:41:31 +0200

    Basic implemenation of View nad ViewModel layer. Added ViewModel.cs

---
+++ ViewModel/ViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Model;
+
+namespace ViewModel
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private ModelAbstractAPI _model;
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
