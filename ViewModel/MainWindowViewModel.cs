4ac50b9e9ecb78e6e019a61f03fcdedfa3fe581e
247813 Tomalak Piotr <247813@edu.p.lodz.pl>
2024-04-05 18:41:50 +0200

    Added MainWindowViewModel which inherit from ViewModel. Added RelayCommand.cs implementation.

---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        private readonly ModelAbstractAPI _modelLayer;
+        private int _ballsAmount;
+        private IList _balls;
+        private readonly int _width;
+        private readonly int _height;
+
+        public MainWindowViewModel(ModelAbstractAPI modelLayer)
+        {
+            _modelLayer = modelLayer;
+            _width = _modelLayer.Width;
+            _height = _modelLayer.Height;
+        }
+
+        public MainWindowViewModel () : this(ModelAbstractAPI.CreateModelAPI()) { }
+
+
+    }
+}
