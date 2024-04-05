d6c996489c23d9949eacb43313252a33fa607dec
hjatczak <hjatczak@gmail.com>
2024-04-05 16:12:55 +0200

    ModelAPI.cs dodane

---
+++ Model/ModelAPI.cs
@@ -0,0 +1,31 @@
+using Logic;
+using System.Collections.ObjectModel;
+
+namespace Model
+{
+    public class ModelAPI : ModelAbstractAPI
+    {
+        private readonly LogicAbstractAPI _logicLayer;
+
+        public ModelAPI(LogicAbstractAPI logicLayer)
+        {
+            _logicLayer = logicLayer ?? LogicAbstractAPI.CreateAPI();
+        }
+        public override int Width => _logicLayer.table.Width;
+        public override int Height => _logicLayer.table.Height;
+        public override ObservableCollection<BallLogic> createBalls(int amount, int radius)
+        {
+           _logicLayer.createBalls(amount, radius);
+           return _logicLayer.balls;
+        }
+        public override void StartSimulation()
+        {
+            _logicLayer.RunSimulation();
+        }
+        public override void StopSimulation()
+        {
+            _logicLayer.StopSimulation();
+        }
+
+    }
+}
